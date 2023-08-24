13de5120035bee54a01d1178cd54c3293fffa02b
or404 <oryak10@gmail.com>
2023-08-24 14:25:29 +0300

    small decoration stuff

--- script.js
+++ script.js
@@ -1,21 +1,21 @@
-const grid = document.querySelector('.grid');
+const grid = document.querySelector('.grid');//creates grid
 
-for(let i = 0 ; i<15 ;i++) {
+for(let i = 0 ; i<16 ;i++) {
 
-    let verticalDiv = document.createElement('div');
-    verticalDiv.style.display = 'flex';
-    grid.appendChild(verticalDiv);
-for(let j = 0 ; j<15 ; j++) {
+    let vertical = document.createElement('div');
+    vertical.style.display = 'flex';
+    grid.appendChild(vertical);
 
+for(let j = 0 ; j<16 ; j++) {
     let horizontalDiv = document.createElement('div');
-    horizontalDiv.style.height = '70px';
-    horizontalDiv.style.width = '170px';
-    horizontalDiv.style.backgroundColor = 'grey';
+    horizontalDiv.style.height = '65px';
+    horizontalDiv.style.width = '149px';
+    horizontalDiv.style.borderWidth = '2px';
+    horizontalDiv.style.borderRadius = '0.3mm';
     horizontalDiv.style.borderStyle = 'solid';
-    verticalDiv.appendChild(horizontalDiv);
-
-
+    vertical.appendChild(horizontalDiv);
+}
 }
 
 
-}
+
