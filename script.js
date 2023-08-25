01ebcdf43023bbe7d0f68e2bb8faee11a41d7c06
or404 <oryak10@gmail.com>
2023-08-25 19:01:17 +0300

    organized and added comments.

--- script.js
+++ script.js
@@ -1,16 +1,18 @@
-const grid = document.querySelector('.grid');//creates grid
-const body = document.querySelector('*');
+const grid = document.querySelector('.grid');
 const clearButton = document.querySelector('#clear-button');
+
 const colorButton = document.querySelector('#change-color');
-const colorMenu = document.querySelectorAll('.color-menu>li');
-let mouseDown = 0;
+const colorMenu = document.querySelector('.color-menu');
+const colorMenuList = document.querySelectorAll('.color-menu>li');
+let colorMenuStatus = false;
 let color = 'red';
 
-body.onmousedown = ()=> {mouseDown=1;};
-body.onmouseup = ()=> {mouseDown=0};
+let mouseDown = 0;
+grid.onmousedown = ()=> {mouseDown=1;};
+grid.onmouseup = ()=> {mouseDown=0};
 
 
-for(let i = 0 ; i<16 ;i++) {
+for(let i = 0 ; i<16 ;i++) {//creates grid
     let vertical = document.createElement('div');
     vertical.style.display = 'flex';
     grid.appendChild(vertical);
@@ -20,8 +22,7 @@ for(let j = 0 ; j<16 ; j++) {
     divs.style.height = '65px';
     divs.style.width = '149px';
     divs.style.borderWidth = '2px';
-    divs.style.borderRadius = '0.2mm';
-    //divs.style.borderStyle = 'solid';
+  //divs.style.borderStyle = 'solid';
     vertical.appendChild(divs);
     divs.addEventListener('click', ()=>{divs.style.backgroundColor = color;})
     divs.addEventListener('mouseover' , ()=> {if(mouseDown===1){divs.style.backgroundColor =color;}})
@@ -29,8 +30,7 @@ for(let j = 0 ; j<16 ; j++) {
 }}
 
 
-
-colorMenu.forEach(li=>{
+colorMenuList.forEach(li=>{//select different color functionality
     li.style.backgroundColor = li.textContent;
 
     li.addEventListener('mouseover', ()=>{
@@ -38,14 +38,24 @@ colorMenu.forEach(li=>{
         li.style.borderColor = 'black';
         li.style.borderWidth = '1mm';
     });
-    li.addEventListener('mouseout' , ()=>{
-        li.style.borderStyle = 'none';
-    });
-
+    li.addEventListener('mouseout' , ()=>{li.style.borderStyle = 'none';});
     li.addEventListener('click' , ()=> {color=li.textContent;});
 });
 
-console.log(color);
+colorButton.addEventListener('click' ,()=>{//open and close color menu
+    if(!colorMenuStatus) {
+    colorMenu.style.display= 'block';
+    colorMenuStatus = true;
+}
+    else {
+        colorMenu.style.display= 'none';
+        colorMenuStatus = false;
+    }
+});
+
+    
+
+
 
 
 
